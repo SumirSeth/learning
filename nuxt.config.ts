00ed991621278f54da7119063180f31ede565495
Sumir Seth <iamsumirseth@gmail.com>
2025-02-20 19:08:13 +0530

    add Supabase module and update dashboard layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
+      supabaseKey: process.env.NUXT_PUBLIC_SUPABASE_KEY,
+      geminiApiKey: process.env.NUXT_PUBLIC_GEMINI_API_KEY
+    }
+  },
+  supabase: {
+    redirect: false,
+    url: process.env.NUXT_PUBLIC_SUPABASE_URL,
+    key: process.env.NUXT_PUBLIC_SUPABASE_KEY
+  },
+  imports:{
+    autoImport: true,
+  },
 })
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <p class="text-5xl font-bold">我的学习</p>
+    <div class="bg-stone-800 min-h-screen flex flex-col">
+      <div class="nav flex justify-center p-5 m-2">
+        <p class="text-6xl font-bold text-white border-t-0 border-b-4 border-amber-400 rounded-lg p-5">Dashboard</p>
+      </div>
+      <div class="blocks flex gap-5 flex-row p-3 m-3 flex-wrap">
+        <div class="block cursor-pointer p-12 text-center content-center  m-2 rounded-lg shadow-md bg-stone-700" v-for="block in blocks" :key="block.title">
+          <p class="block-title text-2xl font-bold text-white">{{ block.title }}</p>
+          <p class="block-description text-lg text-white">{{ block.description }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,20 +19,24 @@
 
 const blocks = ref([
   {
-    title: '我的学习',
-    description: '我的学习',
+    title: 'Mathematics',
+    description: 'Algebra, Calculus, Statistics',
   },
   {
-    title: '我的学习',
-    description: '我的学习',
+    title: 'Science',
+    description: 'Physics, Chemistry, Biology',
   },
   {
-    title: '学习排行',
-    description: '这里是学习排行',
+    title: 'Programming',
+    description: 'Python, JavaScript, Java',
   },
   {
-    title: '个人信息',
-    description: '用户信息',
+    title: 'Languages',
+    description: 'English, Spanish, French',
+  },
+  {
+    title: 'History',
+    description: 'World History, Geography',
   }
 ])
 
